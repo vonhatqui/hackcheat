f937b7b041604f044d7414d85116b23f68fd4564
vonhatqui <quifccpp@gmail.com>
2026-04-18 03:11:54 +0700

    Update Menu.mm

--- Menu.mm
+++ Menu.mm
@@ -1,11 +1,10 @@
 #import <UIKit/UIKit.h>
 
 @interface SavageMenu : UIView
-@property (nonatomic, strong) UIView *mainPanel;
-@property (nonatomic, strong) UIButton *gearBtn;
+@property (nonatomic, strong) UIView *mainBox;
+@property (nonatomic, strong) UIView *sidebar;
 @property (nonatomic, strong) UIView *contentArea;
-@property (nonatomic, strong) UISlider *fovSlider;
-@property (nonatomic, strong) UILabel *fovLabel;
+@property (nonatomic, strong) UIButton *btnS;
 @end
 
 @implementation SavageMenu
@@ -14,104 +13,96 @@
     self = [super initWithFrame:[[UIScreen mainScreen] bounds]];
     if (self) {
         self.userInteractionEnabled = YES;
-        self.backgroundColor = [UIColor clearColor];
-        self.layer.zPosition = 9999;
+        self.layer.zPosition = 99999;
 
-        // 1. NÚT GEAR NỔI (FLOATING)
-        _gearBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        _gearBtn.frame = CGRectMake(40, 150, 55, 55);
-        _gearBtn.backgroundColor = [[UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0] colorWithAlphaComponent:0.8];
-        _gearBtn.layer.cornerRadius = 27.5;
-        _gearBtn.layer.borderWidth = 2.0;
-        _gearBtn.layer.borderColor = [UIColor whiteColor].CGColor;
-        [_gearBtn setTitle:@"⚙️" forState:UIControlStateNormal];
-        _gearBtn.titleLabel.font = [UIFont systemFontOfSize:30];
-        [_gearBtn addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
-        
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-        [_gearBtn addGestureRecognizer:pan];
-        [self addSubview:_gearBtn];
+        // NÚT MỞ MENU (LOGO S TỰ VẼ)
+        _btnS = [UIButton buttonWithType:UIButtonTypeCustom];
+        _btnS.frame = CGRectMake(60, 120, 50, 50);
+        _btnS.backgroundColor = [UIColor colorWithRed:0.0 green:0.2 blue:0.4 alpha:0.8];
+        _btnS.layer.cornerRadius = 8;
+        _btnS.layer.borderWidth = 1.5;
+        _btnS.layer.borderColor = [UIColor cyanColor].CGColor;
+        [_btnS setTitle:@"S" forState:UIControlStateNormal];
+        [_btnS setTitleColor:[UIColor cyanColor] forState:UIControlStateNormal];
+        _btnS.titleLabel.font = [UIFont boldSystemFontOfSize:24];
+        [_btnS addTarget:self action:@selector(toggle) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_btnS];
 
-        // 2. DASHBOARD CHÍNH
-        _mainPanel = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 420)];
-        _mainPanel.center = self.center;
-        _mainPanel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.95];
-        _mainPanel.layer.cornerRadius = 25;
-        _mainPanel.layer.borderWidth = 2.5;
-        _mainPanel.layer.borderColor = [UIColor colorWithRed:0.0 green:0.6 blue:1.0 alpha:1.0].CGColor;
-        _mainPanel.hidden = YES;
-        [self addSubview:_mainPanel];
+        // KHUNG MENU CHÍNH (GIỐNG ẢNH)
+        _mainBox = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 540, 350)];
+        _mainBox.center = self.center;
+        _mainBox.backgroundColor = [UIColor colorWithRed:0.01 green:0.02 blue:0.05 alpha:0.98];
+        _mainBox.layer.cornerRadius = 12;
+        _mainBox.layer.borderWidth = 1;
+        _mainBox.layer.borderColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:0.3].CGColor;
+        _mainBox.hidden = YES;
+        [self addSubview:_mainBox];
 
-        // TAB BAR
-        NSArray *tabs = @[@"Main", @"Visual", @"Cài Đặt"];
+        // THANH BÊN (SIDEBAR)
+        _sidebar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 150, 350)];
+        _sidebar.backgroundColor = [UIColor colorWithRed:0.0 green:0.01 blue:0.03 alpha:0.6];
+        [_mainBox addSubview:_sidebar];
+
+        NSArray *tabs = @[@"AIMBOT", @"VISUALS", @"ANTIBAN", @"SETTINGS"];
         for (int i = 0; i < tabs.count; i++) {
-            UIButton *tab = [UIButton buttonWithType:UIButtonTypeCustom];
-            tab.frame = CGRectMake(i * (320/3.0), 0, 320/3.0, 50);
-            [tab setTitle:tabs[i] forState:UIControlStateNormal];
-            tab.titleLabel.font = [UIFont boldSystemFontOfSize:15];
-            tab.tag = i;
-            [tab addTarget:self action:@selector(switchTab:) forControlEvents:UIControlEventTouchUpInside];
-            [_mainPanel addSubview:tab];
+            UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+            btn.frame = CGRectMake(0, i * 65 + 30, 150, 50);
+            [btn setTitle:tabs[i] forState:UIControlStateNormal];
+            btn.titleLabel.font = [UIFont boldSystemFontOfSize:11];
+            [btn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+            btn.tag = i;
+            [btn addTarget:self action:@selector(tabChanged:) forControlEvents:UIControlEventTouchUpInside];
+            [_sidebar addSubview:btn];
         }
 
-        _contentArea = [[UIView alloc] initWithFrame:CGRectMake(0, 55, 320, 360)];
-        [_mainPanel addSubview:_contentArea];
-        [self loadTabData:0];
+        _contentArea = [[UIView alloc] initWithFrame:CGRectMake(165, 15, 360, 320)];
+        [_mainBox addSubview:_contentArea];
+        [self loadAimbotUI];
     }
     return self;
 }
 
-- (void)switchTab:(UIButton *)s { [self loadTabData:(int)s.tag]; }
-
-- (void)loadTabData:(int)index {
+- (void)loadAimbotUI {
     for (UIView *v in _contentArea.subviews) [v removeFromSuperview];
-    if (index == 0) {
-        [self addLabel:@"AUTO AIMBOT (REAL)" y:20];
-        [self addSwitch:101 y:20];
-        [self addLabel:@"AIM FOV" y:80];
-        _fovSlider = [[UISlider alloc] initWithFrame:CGRectMake(20, 110, 240, 30)];
-        _fovSlider.tag = 999;
-        _fovSlider.minimumValue = 10; _fovSlider.maximumValue = 360; _fovSlider.value = 180;
-        [_fovSlider addTarget:self action:@selector(fovCh:) forControlEvents:UIControlEventValueChanged];
-        [_contentArea addSubview:_fovSlider];
-        _fovLabel = [[UILabel alloc] initWithFrame:CGRectMake(270, 110, 40, 30)];
-        _fovLabel.text = @"180"; _fovLabel.textColor = [UIColor cyanColor];
-        [_contentArea addSubview:_fovLabel];
-        [self addLabel:@"GHIM ĐẦU (LOCK)" y:170];
-        [self addSwitch:102 y:170];
-    } else if (index == 1) {
-        [self addLabel:@"ESP LINE" y:20]; [self addSwitch:201 y:20];
-        [self addLabel:@"ESP BOX" y:80]; [self addSwitch:202 y:80];
-    } else {
-        [self addLabel:@"DEVICE: Savage iPhone" y:20];
-        [self addLabel:@"VERSION: OB52 - FINAL" y:60];
-        [self addLabel:@"STATUS: BYPASS ACTIVE" y:100];
-    }
-}
+    
+    UILabel *h = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300, 25)];
+    h.text = @"AIMBOT | SYSTEM CONFIG";
+    h.textColor = [UIColor cyanColor];
+    h.font = [UIFont boldSystemFontOfSize:13];
+    [_contentArea addSubview:h];
 
-- (void)addLabel:(NSString *)t y:(float)y {
-    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(20, y, 220, 30)];
-    l.text = t; l.textColor = [UIColor whiteColor];
-    [_contentArea addSubview:l];
-}
+    float y = 45;
+    [self addRow:@"Aimbot (Lock Head)" tag:101 y:&y];
+    [self addRow:@"Draw FOV Circle" tag:301 y:&y];
+    [self addRow:@"Ignore AI Players" tag:103 y:&y];
+    [self addRow:@"Visible Check" tag:104 y:&y];
+    
+    UILabel *sl = [[UILabel alloc] initWithFrame:CGRectMake(0, y, 200, 20)];
+    sl.text = @"FOV RANGE SENSITIVITY"; sl.textColor = [UIColor whiteColor];
+    sl.font = [UIFont systemFontOfSize:10];
+    [_contentArea addSubview:sl]; y+=25;
 
-- (void)addSwitch:(int)tag y:(float)y {
-    UISwitch *s = [[UISwitch alloc] initWithFrame:CGRectMake(250, y, 0, 0)];
-    s.tag = tag;
-    s.onTintColor = [UIColor colorWithRed:0.0 green:0.6 blue:1.0 alpha:1.0];
+    UISlider *s = [[UISlider alloc] initWithFrame:CGRectMake(0, y, 320, 30)];
+    s.tag = 999; s.minimumValue = 20; s.maximumValue = 400; s.value = 150;
+    s.minimumTrackTintColor = [UIColor cyanColor];
     [_contentArea addSubview:s];
 }
 
-- (void)fovCh:(UISlider *)s { _fovLabel.text = [NSString stringWithFormat:@"%d", (int)s.value]; }
-- (void)toggleMenu { _mainPanel.hidden = !_mainPanel.hidden; }
-- (void)handlePan:(UIPanGestureRecognizer *)p {
-    CGPoint t = [p translationInView:self];
-    _gearBtn.center = CGPointMake(_gearBtn.center.x + t.x, _gearBtn.center.y + t.y);
-    [p setTranslation:CGPointZero inView:self];
+- (void)addRow:(NSString*)name tag:(int)tag y:(float*)y {
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(40, *y, 240, 30)];
+    l.text = [name uppercaseString]; l.textColor = [UIColor whiteColor];
+    l.font = [UIFont systemFontOfSize:11];
+    [_contentArea addSubview:l];
+    
+    UISwitch *s = [[UISwitch alloc] initWithFrame:CGRectMake(0, *y, 0, 0)];
+    s.tag = tag; s.onTintColor = [UIColor cyanColor];
+    s.transform = CGAffineTransformMakeScale(0.6, 0.6);
+    [_contentArea addSubview:s];
+    *y += 40;
 }
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-    UIView *v = [super hitTest:point withEvent:event];
-    return (v == self) ? nil : v;
-}
+
+- (void)toggle { _mainBox.hidden = !_mainBox.hidden; }
+- (void)tabChanged:(UIButton*)b { if(b.tag == 0) [self loadAimbotUI]; }
 @end
+
 
