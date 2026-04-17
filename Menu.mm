0835fc73709002faa65c5087d40a03e587b50b12
vonhatqui <quifccpp@gmail.com>
2026-04-18 01:44:59 +0700

    Update Menu.mm

--- Menu.mm
+++ Menu.mm
@@ -8,37 +8,26 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        // NỀN ĐEN KHÓI VÀ VIỀN XANH DƯƠNG NEON
         self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.85];
         self.layer.cornerRadius = 20;
         self.layer.borderWidth = 2.5;
         self.layer.borderColor = [UIColor colorWithRed:0.0 green:0.6 blue:1.0 alpha:1.0].CGColor;
         
-        // HEADER ĐẲNG CẤP
         UIView *header = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 45)];
         header.backgroundColor = [UIColor colorWithRed:0.0 green:0.4 blue:0.9 alpha:0.8];
         UILabel *title = [[UILabel alloc] initWithFrame:header.bounds];
-        title.text = @"SAVAGE BLUE 2026 - PREMIUM";
+        title.text = @"SAVAGE BLUE - NO KEY";
         title.textColor = [UIColor whiteColor];
         title.textAlignment = NSTextAlignmentCenter;
-        title.font = [UIFont fontWithName:@"Helvetica-Bold" size:16];
+        title.font = [UIFont boldSystemFontOfSize:16];
         [self addSubview:header];
-        
-        // STATUS - KÍCH HOẠT SẴN (KHÔNG CẦN KEY)
-        UILabel *st = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, frame.size.width, 20)];
-        st.text = @"[ SYSTEM: BYPASS SUCCESS ]";
-        st.textColor = [UIColor cyanColor];
-        st.font = [UIFont systemFontOfSize:11];
-        st.textAlignment = NSTextAlignmentCenter;
-        [self addSubview:st];
 
-        [self setupSwitches];
+        UILabel *status = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, frame.size.width, 20)];
+        status.text = @"[ BYPASS ACTIVE ]";
+        status.textColor = [UIColor cyanColor];
+        status.textAlignment = NSTextAlignmentCenter;
+        [self addSubview:status];
     }
     return self;
-}
-
-- (void)setupSwitches {
-    // Đại ca có thể thêm các nút gạt AIMBOT, ESP tại đây
-    // Em đã code sẵn logic trong Tweak.xm bên dưới
 }
 @end
